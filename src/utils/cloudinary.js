884b8e9f6645eec8fbdd869f9cf62a8f099ffdf3
TheRitik <ritik2031129@akgec.ac.in>
2024-01-06 18:25:08 +0530

    SETUP COMPLETE FOR PROJECT

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,26 @@
+import fs from "fs";
+import {v2 as cloudinary} from 'cloudinary';
+          
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key:process.env.CLOUDINARY_API_KEY , 
+  api_secret:process.env.CLOUDINARY_API_SECRET 
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath){
+            return null;
+        }
+        const response = cloudinary.uploader.upload(localFilePath,{
+            resource_type:"auto"
+        })
+        //file uploaded successfully
+        console.log('File uploaded successfully', (await response).url);
+        return response;
+    } catch (error) {
+        fs.unlinkSync(localFilePath) // remove locally saved temp file as upload failed
+    }
+}
+
+export {uploadOnCloudinary} ;
